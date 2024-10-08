c1ec439ac983e5e02d6c06b4241d24d75467e424
zhuhaichao518 <zhuhaichao518@gmail.com>
2024-10-09 07:32:34 +0800

    add web support for no cursor game

--- lib/hardware_simulator_web.dart
+++ lib/hardware_simulator_web.dart
@@ -3,18 +3,19 @@
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
 
+import 'package:hardware_simulator/hardware_simulator_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:web/web.dart' as web;
-
-import 'hardware_simulator_platform_interface.dart';
+import 'dart:html';
+import 'dart:js_util' as js_util;
 
 /// A web implementation of the HardwareSimulatorPlatform of the HardwareSimulator plugin.
-class HardwareSimulatorWeb extends HardwareSimulatorPlatform {
+class HardwareSimulatorPluginWeb extends HardwareSimulatorPlatform {
   /// Constructs a HardwareSimulatorWeb
-  HardwareSimulatorWeb();
+  HardwareSimulatorPluginWeb();
 
   static void registerWith(Registrar registrar) {
-    HardwareSimulatorPlatform.instance = HardwareSimulatorWeb();
+    HardwareSimulatorPlatform.instance = HardwareSimulatorPluginWeb();
   }
 
   /// Returns a [String] containing the version of the platform.
@@ -22,5 +23,43 @@ class HardwareSimulatorWeb extends HardwareSimulatorPlatform {
   Future<String?> getPlatformVersion() async {
     final version = web.window.navigator.userAgent;
     return version;
+  }
+
+  @override
+  Future<void> lockCursor() async {
+    js_util.callMethod(document.body!, 'requestPointerLock', []);
+  }
+
+  @override
+  Future<void> unlockCursor() async {
+    js_util.callMethod(document, 'exitPointerLock', []);
+  }
+
+  bool isinitialized = false;
+
+  void init() {
+    // 监听鼠标移动事件
+    document.onMouseMove.listen((event) {
+      // 使用 movementX 和 movementY 获取相对位移
+      double dx = event.movement.x.toDouble();
+      double dy = event.movement.y.toDouble();
+      for (var callback in cursorMovedCallbacks){
+        callback(dx,dy);
+      }
+    });
+    isinitialized = true;
+  }
+
+  final List<CursorMovedCallback> cursorMovedCallbacks = [];
+
+  @override
+  void addCursorMoved(CursorMovedCallback callback) {
+    if (!isinitialized) init();
+    cursorMovedCallbacks.add(callback);
+  }
+
+  @override
+  void removeCursorMoved(CursorMovedCallback callback) {
+    cursorMovedCallbacks.remove(callback);
   }
 }
