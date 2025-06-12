a8b1049ac9a9fc16551756e63917c81dca818ee1
zhuhaichao518 <zhuhaichao518@gmail.com>
2025-06-12 11:28:01 +0800

    fix gradle build

--- example/android/app/src/main/kotlin/com/cloudplayplus/hardware_simulator_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/cloudplayplus/hardware_simulator_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.cloudplayplus.hardware_simulator_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
